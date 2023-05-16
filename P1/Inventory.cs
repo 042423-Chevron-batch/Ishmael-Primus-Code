c200e950ae096224032f3f3c67b9514bb5d9095e
IshmaelPrimus <ishmaelprimus@yahoo.com>
2023-05-16 14:56:08 -0500

    added comments

--- P1/Inventory.cs
+++ P1/Inventory.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-public class GroceryItem
+/// <summary>
+/// SET PUBLIC CLASS
+/// </summary>
+public class Inventory
 {
     public string Name { get; set; }
     public decimal Price { get; set; }
@@ -15,7 +18,7 @@ public class GroceryItem
         set { quantity = value; }
     }
 
-    public GroceryItem(string name, decimal price, int quantity)
+    public Inventory(string name, decimal price, int quantity)
     {
         Name = name;
         Price = price;
@@ -28,40 +31,60 @@ public class GroceryItem
     }
 }
 
-public class GroceryList
+/// <summary>
+/// INVENTORY LIST
+/// </summary>
+public class Inventory
 {
-    private List<GroceryItem> items;
+    private List<Inventory> items;
 
-    public GroceryList()
+    public Inventory()
     {
-        items = new List<GroceryItem>();
+        items = new List<Inventory>();
     }
 
+/// <summary>
+/// ADD ITEM TO THE LIST
+/// </summary>
+/// <param name="name"></param>
+/// <param name="price"></param>
+/// <param name="quantity"></param>
     public void AddItem(string name, decimal price, int quantity)
     {
-        GroceryItem item = new GroceryItem(name, price, quantity);
+        Inventory item = new InventoryItem(name, price, quantity);
         items.Add(item);
     }
 
-    public void RemoveItem(GroceryItem item)
+/// <summary>
+/// REMOVE ITEM FROM THE LIST
+/// </summary>
+/// <param name="item"></param>
+    public void RemoveItem(Inventory item)
     {
         items.Remove(item);
     }
-
+    
+/// <summary>
+/// TOTAL PRICE OF EACH TIME
+/// </summary>
+/// <returns></returns>
     public decimal CalculateTotalPrice()
     {
         decimal totalPrice = 0;
-        foreach (GroceryItem item in items)
+        foreach (InventoryItem item in items)
         {
             totalPrice += item.TotalPrice();
         }
         return totalPrice;
     }
 
-    public void PrintGroceryList()
+/// <summary>
+/// PRINT INVENTORY NAME,PRICE,QUANTITY AND TOTAL PRICE
+/// </summary>
+    public void PrintInventory()
     {
         Console.WriteLine("Grocery List:");
-        foreach (GroceryItem item in items)
+        foreach (InventoryItem item in items)
         {
             Console.WriteLine($"Item: {item.Name}\tPrice: {item.Price}\tQuantity: {item.Quantity}\tTotal: {item.TotalPrice()}");
         }
