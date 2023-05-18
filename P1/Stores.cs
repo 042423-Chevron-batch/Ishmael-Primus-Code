0d0f8a192e24beb552f5d5da17d278102b96f284
IshmaelPrimus <ishmaelprimus@yahoo.com>
2023-05-18 16:17:31 -0500

    major changes

--- P1/Stores.cs
+++ P1/Stores.cs
@@ -13,4 +13,75 @@ namespace P1
         Kroger = 1,
         HEB = 2,
     }
+    public enum Locations
+    {
+        Sugarland,
+        Katy,
+        Richmond
+    }
+
+    public class WalmartSugarland
+    {
+        public Stores Store { get; set; }
+        public Locations Location { get; set; } 
+        public MilkInventory milkInventory { get; set; }
+        public BreadInventory breadInventory { get; set; }
+        public CerealInventory cerealInventory { get; set; }
+
+        public WalmartSugarland ()
+        {
+        store = Stores.Walmart;
+        Location = Locations.Sugarland;
+        milkInventory = MilkInventory.product;
+        milkInventory = MilkInventory.quantity;
+        breadInventory = BreadInventory.product;
+        breadInventory = BreadInventory.quantity;
+        cerealInventory = CerealInventory.product;
+        cerealInventory = CerealInventory.quantity;
+        }
+    }
+
+    public class KrogerRichmond
+    {
+        public Stores Store { get; set; }
+        public Locations Location { get; set; } 
+        public IcecreamInventory icecreamInventory { get; set; }
+        public PizzaInventory pizzaInventory { get; set; }
+        public CakeInventory  cakeInventory { get; set; }
+
+        public KrogerRichmond ()
+        {
+        store = Stores.Kroger;
+        Location = Locations.Richmond;
+        icecreamInventory = IcecreamInventory.product;
+        icecreamInventory = IcecreamInventory.quantity;
+        pizzaInventory = PizzaInventory.product;
+        pizzaInventory = PizzaInventory.quantity;
+        cakeInventory = CakeInventory.product;
+        cakeInventory = CakeInventory.quantity;
+        }
+    }
+
+    public class HEBKaty
+    {
+        public Stores Store { get; set; }
+        public Locations Location { get; set; } 
+        public SaladInventory saladInventory { get; set; }
+        public ChickenInventory chickenInventory { get; set; }
+        public FishInventory  fishInventory { get; set; }
+
+        public HEBKaty ()
+        {
+        store = Stores.HEB;
+        Location = Locations.Katy;
+        saladInventory = SaladInventory.product;
+        saladInventory = SaladInventory.quantity;
+        chickenInventory = ChickenInventory.product;
+        chickenInventory = ChickenInventory.quantity;
+        fishInventory = FishInventory.product;
+        fishInventory = FishInventory.quantity;
+        }
+    }
+
+    
 }
