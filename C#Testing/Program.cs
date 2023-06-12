f0afaa475773b654fb9d4db44447ea4b9a8ad5bc
ishmaelprimus <ishmaelprimus@yahoo.com>
2023-06-11 19:20:44 -0500

    changes

---
+++ C#Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+static void IfStatement(string[] args)
+{
+    if (args.Length == 0)
+    {
+        Console.WriteLine("There are no arguments.");
+    }
+    else
+    {
+        Console.WriteLine("There is at least one argument.");
+    }
+}
